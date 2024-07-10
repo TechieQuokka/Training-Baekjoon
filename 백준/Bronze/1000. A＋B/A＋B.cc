8db5979170e43eff7e26fdf76d594b68410183bb
Quokka <everestfesf@gmail.com>
2024-07-10 17:39:50 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main (int argc, char* argv[]) {
+	
+	int lvalue = 0, rvalue = 0;
+	scanf ("%d%d", &lvalue, &rvalue);
+	printf ("%d\n", lvalue + rvalue);
+	return 0;
+}
