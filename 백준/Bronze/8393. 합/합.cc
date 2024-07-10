e80516ab29927d4ad492d287ff6d647214c16048
Quokka <everestfesf@gmail.com>
2024-07-10 17:41:06 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main (int argc, char* argv[]) {
+
+    int number = 0;
+    cin >> number;
+    cout << number * (number + 1) / 2 << endl;
+    return 0;
+}
