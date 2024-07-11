2108d490569b1cfe62ca1ec1131a35588b37b725
Quokka <everestfesf@gmail.com>
2024-07-11 16:32:20 +0900

    [Bronze V] Title: 큰 수 A+B, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.cc
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define SIZE 10001
+
+void Sum (char lvalue[], char rvalue[], char result[]);
+
+int main (int argc, char* argv[]) {
+    
+    char lvalue[SIZE] = { 0, }, rvalue[SIZE] = { 0, };
+    scanf ("%s%s", lvalue, rvalue);
+    
+    char result[SIZE] = { 0, };
+    Sum (lvalue, rvalue, result);
+    printf ("%s", result);
+    return 0;
+}
+
+bool IsDigit (char character) {
+    
+    return character >= '0' && character <= '9';
+}
+
+unsigned char ToNumber (char character) {
+    
+    if (!IsDigit (character)) return (unsigned char)255;
+    return (unsigned char) (character - '0');
+}
+
+char ToCharacter (unsigned char number) {
+    
+    if (number > 9) return (char) (-128);
+    return (char)number + '0';
+}
+
+void Swap (const void* argument1, const void* argument2, size_t type) {
+    
+    unsigned char* element1 = (unsigned char*) argument1, * element2 = (unsigned char*) argument2;
+    
+    size_t byte = 0LL;
+    for (byte = 0LL; byte < type; byte++) {
+        
+        unsigned char temporary = element1[byte];
+        element1[byte] = element2[byte];
+        element2[byte] = temporary;
+    }
+    return;
+}
+
+void Reverse (char* source, int length) {
+    
+    if (source == NULL) return;
+    
+    int left = 0, right = 0;
+    for (left = 0, right = length - 1; left < right; left++, right--) {
+        
+        Swap ((char*)source + left, (char*)source + right, sizeof (char));
+    }
+    return;
+}
+
+void Sum (char lvalue[], char rvalue[], char result[]) {
+    
+    if (lvalue == NULL || rvalue == NULL) return;
+    
+    unsigned char buffer = 0U;
+    const unsigned char ten = (unsigned char) 10;
+    
+    int lpivot = strlen (lvalue) - 1, rpivot = strlen (rvalue) - 1;
+    int repeat = 0, index = 0;
+    for (repeat = lpivot < rpivot ? lpivot + 1 : rpivot + 1; repeat; repeat--, lpivot--, rpivot--, index++) {
+        
+        unsigned char leftNumber = ToNumber(lvalue[lpivot]), rightNumber = ToNumber(rvalue[rpivot]);
+        unsigned char calculate = leftNumber + rightNumber + buffer;
+        result[index] = ToCharacter (calculate % ten);
+        buffer = calculate / ten;
+    }
+    
+    int pivot = 0; char* biggest = NULL;
+    if (strlen (lvalue) < strlen (rvalue)) biggest = rvalue, pivot = rpivot;
+    else biggest = lvalue, pivot = lpivot;
+    
+    while (pivot >= 0) {
+        
+        unsigned char number = ToNumber (biggest[pivot--]);
+        unsigned char calculate = number + buffer;
+        result[index++] = ToCharacter (calculate % ten);
+        buffer = calculate / ten;
+    }
+    if (buffer) result[index++] = '1';
+    result[index] = '\0';
+    
+    Reverse (result, index);
+    return;
+}
