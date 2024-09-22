2c2919656d25b18a11e036ed7b015a6fa44661af
Quokka <everestfesf@gmail.com>
2024-09-22 21:34:02 +0900

    [Silver II] Title: 색종이 만들기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.cc
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.cc
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-int CountPaper (int paper[128][128], int length, int x, int y, bool (*compare) (int[128][128], int, int));
+int CountPaper (int paper[][128], int x1, int y1, int x2, int y2, int size, bool (*compare) (int[][128], int, int));
 
 bool black (int paper[128][128], int row, int column) {
     
@@ -14,7 +14,7 @@ bool white (int paper[128][128], int row, int column) {
 }
 
 int main (int argc, char* argv[]) {
-    
+
     int size = 0;
     scanf ("%d", &size);
     
@@ -28,38 +28,34 @@ int main (int argc, char* argv[]) {
         }
     }
     
-    int result = CountPaper (buffer, size, 0, 0, &white);
+    int result = CountPaper (buffer, 0, 0, size, size, size, &white);
     printf ("%d\n", result);
-    
-    result = CountPaper (buffer, size, 0, 0, &black);
+
+    result = CountPaper (buffer, 0, 0, size, size, size, &black);
     printf ("%d\n", result);
     return 0;
 }
 
-bool Check (int paper[128][128], int length, int x, int y, bool (*compare) (int[128][128], int, int)) {
+bool Check (int paper[128][128], int x1, int y1, int x2, int y2, bool (*compare) (int[128][128], int, int)) {
 
-    int rowLength = length + x;
-    int columnLength = length + y;
-    
     int row = 0, column = 0;
-    for (row = x; row < rowLength; row++) {
-        for (column = y; column < columnLength; column++) {
-            
+    for (row = x1; row < x2; row++) {
+        for (column = y1; column < y2; column++) {
             if (!compare (paper, row, column)) return false;
         }
     }
     return true;
 }
 
-int CountPaper (int paper[128][128], int length, int x, int y, bool (*compare) (int[128][128], int, int)) {
+int CountPaper (int paper[][128], int x1, int y1, int x2, int y2, int size, bool (*compare) (int[][128], int, int)) {
+
+    if (size == 0) return 0;
+    else if (Check (paper, x1, y1, x2, y2, compare)) return 1;
     
-    if (Check (paper, length, x, y, compare)) return 1;
-    if (length == 1) return (int) (compare (paper, x, y));
-    
-    int topLeft = CountPaper (paper, length / 2, x, y, compare);
-    int topRight = CountPaper (paper, length / 2, x, y + length / 2, compare);
-    int bottomLeft = CountPaper (paper, length / 2, x + length / 2, y, compare);
-    int bottomRight = CountPaper (paper, length / 2, x + length / 2, y + length / 2, compare);
-    
-    return topLeft + topRight + bottomLeft + bottomRight;
+    int count = 0;
+    count += CountPaper (paper, x1, y1, x1 + (size / 2), y1 + (size / 2), size / 2, compare);
+    count += CountPaper (paper, x1, y1 + (size / 2), x2 - (size / 2), y2, size / 2, compare);
+    count += CountPaper (paper, x1 + (size / 2), y1, x2, y2 - (size / 2), size / 2, compare);
+    count += CountPaper (paper, x1 + (size / 2), y1 + (size / 2), x2, y2, size / 2, compare);
+    return count;
 }
