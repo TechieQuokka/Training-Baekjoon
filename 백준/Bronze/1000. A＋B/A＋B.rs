67d59cd561caf4e8901b1ce1c989d5cfb6e8e3d2
Quokka <everestfesf@gmail.com>
2024-10-03 06:55:45 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 13204 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.rs
@@ -0,0 +1,14 @@
+use std::io;
+
+fn main() {
+
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input).expect("");
+
+    let mut iterate = input.trim().split_whitespace();
+    let left: i32 = iterate.next().unwrap().parse().expect("");
+    let right: i32 = iterate.next().unwrap().parse().expect("");
+
+    println!("{}", left + right)
+}
