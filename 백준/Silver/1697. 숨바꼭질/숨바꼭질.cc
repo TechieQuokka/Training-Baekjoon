db1f5b3168f8f2fe4682205a48754d6e0d21492e
Quokka <everestfesf@gmail.com>
2025-03-08 11:27:17 +0900

    [Silver I] Title: 숨바꼭질, Time: 32 ms, Memory: 4184 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+struct NodeType {
+	
+	int Value;
+	int Depth;
+};
+
+int solution (int start, int end);
+
+int main (int argc, char* argv[]) {
+	
+	int start = 5, end = 17;
+	scanf ("%d%d", &start, &end);
+	int result = solution (start, end);
+	cout << result;
+	return 0;
+}
+
+int solution (int start, int end) {
+	
+	static bool plane[1000001] = { 0, };
+	queue<struct NodeType> buffer;
+	
+	int depth = 0;
+	
+	buffer.push({start, 0});
+	while (!buffer.empty()) {
+		
+		struct NodeType current = buffer.front(); buffer.pop();
+		if (current.Value < 0 || current.Value > 1000000 || plane[current.Value] == true) continue;
+		else if (current.Value == end) {
+			
+			depth = current.Depth;
+			break;
+		}
+		
+		plane[current.Value] = true;
+		depth = current.Depth;
+		
+		buffer.push ({current.Value - 1, current.Depth + 1});
+		buffer.push ({current.Value + 1, current.Depth + 1});
+		buffer.push ({current.Value * 2, current.Depth + 1});
+	}
+	
+	return depth;
+}
