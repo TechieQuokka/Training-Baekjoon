fe44b93f959f1a87f9e19cbb55bab4507d13332b
Quokka <everestfesf@gmail.com>
2025-04-23 00:22:21 +0900

    [Gold IV] Title: 두 배, Time: 48 ms, Memory: 5040 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/31963. 두 배/두 배.cc
+++ 백준/Gold/31963. 두 배/두 배.cc
@@ -1,33 +1,33 @@
 #include <stdio.h>
+#include <math.h>
 
-int main() {
-    int N;
-    scanf("%d", &N);
+int main (int argc, char* argv[]) {
+  
+  int length = 0;
+  scanf ("%d", &length);
+  
+  long long* buffer = (long long*) calloc (length, sizeof (long long));
+  for (int index = 0; index < length; index++) {
     
-    long long prev, curr;
-    scanf("%lld", &prev);
+    scanf ("%lld", buffer + index);
+  }
+  
+  long long* counter = (long long*) calloc (length, sizeof (long long));
+  long long result = 0;
+  
+  for (int index = 1; index < length; index++) {
     
-    long long count = 0;
-    for (int i = 1; i < N; i++) {
-        scanf("%lld", &curr);
-        
-        if (prev <= curr) {
-            prev = curr;
-        } else {
-            long long ratio = (prev + curr - 1) / curr;
-            int exponent = 0;
-            long long power = 1;
-            
-            while (power < ratio) {
-                power <<= 1;
-                exponent++;
-            }
-            
-            count += exponent;
-            prev = curr * power;
-        }
+    long long calculate = ceil(log2((double)buffer[index - 1] / buffer[index])) + counter[index - 1];
+    if (calculate > 0) {
+      
+      counter[index] = calculate;
+      result += calculate;
     }
-    
-    printf("%lld\n", count);
-    return 0;
+  }
+  
+  printf ("%lld\n", result);
+  
+  free ((long long*) counter);
+  free ((long long*) buffer);
+  return 0;
 }
